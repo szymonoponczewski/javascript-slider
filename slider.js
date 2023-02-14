2cc9a8f17d10c28e282f166609fd16d7c913dabd
Szymon Oponczewski <szymon_o@mac.com>
2023-02-14 21:46:48 +0100

    first commit

---
+++ slider.js
@@ -0,0 +1,76 @@
+// Deklaracja zmiennych
+
+let activeSlideNumber = 1;
+
+let arrowLeft = document.querySelector(".arrow-left");
+let arrowRight = document.querySelector(".arrow-right");
+
+let dot1 = document.querySelector("#dot1");
+let dot2 = document.querySelector("#dot2");
+let dot3 = document.querySelector("#dot3");
+
+let slide1 = document.querySelector("#slide1");
+let slide2 = document.querySelector("#slide2");
+let slide3 = document.querySelector("#slide3");
+
+// Funkcja usuwająca klasę .active
+let hideActiveSlide = () => {
+  let activeElement = document.querySelector(".active");
+  activeElement.classList.remove("active");
+};
+
+// Funkcja uruchamia funkcję hideActiveSlide oraz dodaje do elementu #slide klasę .active
+let showSlide = (slideNumber) => {
+  hideActiveSlide();
+  document.querySelector("#slide" + slideNumber).classList.add("active");
+};
+
+let showNextSlide = () => {
+  if (activeSlideNumber === 3) {
+    activeSlideNumber = 1;
+  } else {
+    activeSlideNumber = activeSlideNumber + 1;
+  }
+  showSlide(activeSlideNumber);
+};
+
+let showPreviousSlide = () => {
+  if (activeSlideNumber === 1) {
+    activeSlideNumber = 3;
+  } else {
+    activeSlideNumber = activeSlideNumber - 1;
+  }
+  showSlide(activeSlideNumber);
+};
+
+// Zastąpione pętlą for
+// let showSlide1 = () => {
+//   showSlide(1);
+//   activeSlideNumber = 1;
+// };
+
+// let showSlide2 = () => {
+//   showSlide(2);
+//   activeSlideNumber = 2;
+// };
+
+// let showSlide3 = () => {
+//   showSlide(3);
+//   activeSlideNumber = 3;
+// };
+
+for (let i = 1; i <= 3; i++) {
+  let showSlideX = () => {
+    activeSlideNumber = i;
+    showSlide(i);
+  };
+  document.querySelector("#dot" + i).addEventListener("click", showSlideX);
+}
+
+// Zastąpione powyzszym queryySelectorem
+// dot1.addEventListener("click", showSlide1);
+// dot2.addEventListener("click", showSlide2);
+// dot3.addEventListener("click", showSlide3);
+
+arrowRight.addEventListener("click", showNextSlide);
+arrowLeft.addEventListener("click", showPreviousSlide);
